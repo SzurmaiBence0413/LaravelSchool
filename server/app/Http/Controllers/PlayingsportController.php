50876f77125e61b1e2c85722cfa1eabf0a81cda2
SzurmaiBence0413 <szurmaibence56@gmail.com>
2025-11-24 12:56:40 +0100

    playingsports crud fájlok kész

---
+++ server/app/Http/Controllers/PlayingsportController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\playingsport;
+use App\Http\Requests\StoreplayingsportRequest;
+use App\Http\Requests\UpdateplayingsportRequest;
+
+class PlayingsportController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreplayingsportRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(playingsport $playingsport)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateplayingsportRequest $request, playingsport $playingsport)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(playingsport $playingsport)
+    {
+        //
+    }
+}
