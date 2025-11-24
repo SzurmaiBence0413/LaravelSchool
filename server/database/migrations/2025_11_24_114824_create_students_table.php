4f94526e5cec74b53f7a2bcaf546a1701295fdfe
SzurmaiBence0413 <szurmaibence56@gmail.com>
2025-11-24 12:49:00 +0100

    sport migráció kész

--- server/database/migrations/2025_11_24_114824_create_students_table.php
+++ server/database/migrations/2025_11_24_114824_create_students_table.php
@@ -11,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('sports', function (Blueprint $table) {
+        Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->string('sportNev', 50)->notNull();
             $table->timestamps();
         });
     }
@@ -23,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('sports');
+        Schema::dropIfExists('students');
     }
 };
